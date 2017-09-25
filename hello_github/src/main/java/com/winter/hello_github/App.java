28c5d9eecfc2fc8b17beb97bdf00e264bc6dd27a
winter <ysd0309@126.com>
2017-09-25 22:35:41 +0800

    首次提交代码

---
+++ hello_github/src/main/java/com/winter/hello_github/App.java
@@ -0,0 +1,13 @@
+package com.winter.hello_github;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello GitHub!" );
+    }
+}
