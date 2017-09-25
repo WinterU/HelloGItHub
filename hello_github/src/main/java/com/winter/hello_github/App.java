41ad7c489e18a8714c7046faf831cb6478e6a989
winter <ysd0309@126.com>
2017-09-25 22:52:05 +0800

    add sysdate

--- hello_github/src/main/java/com/winter/hello_github/App.java
+++ hello_github/src/main/java/com/winter/hello_github/App.java
@@ -1,13 +1,14 @@
 package com.winter.hello_github;
 
+import java.util.Date;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello GitHub!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub!");
+		System.out.println(new Date());
+	}
 }
